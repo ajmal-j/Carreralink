baa698b3044e694835601ca3e73969129c4b13dc
ajmal-j <ajmaljaleel7860@gmail.com>
2024-04-19 02:15:44 +0530

    fix (chat) : layout fix in chat component.

--- client/src/app/dashboard/recruiter/(dashboard)/messages/[id]/page.tsx
+++ client/src/app/dashboard/recruiter/(dashboard)/messages/[id]/page.tsx
@@ -7,21 +7,23 @@ import { toast } from "@/components/ui/use-toast";
 import { getMessage } from "@/lib/utils";
 import { getMessages, sendMessages } from "@/services/chat.service";
 import { formatDistanceToNow } from "date-fns";
-import { Loader } from "lucide-react";
+import { Loader, Send } from "lucide-react";
 import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
 
 export default function Chat({ params: { id } }: { params: { id: string } }) {
   const [messages, setMessages] = useState<IMessage[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
   const [input, setInput] = useState<string>("");
   const { socket } = useSocket();
   const divRef = useRef<HTMLDivElement>(null);
 
   const scrollToBottom = () => {
     if (divRef.current) {
-      divRef.current.scrollIntoView({ behavior: "smooth" });
+      divRef.current.scrollTop = divRef.current.scrollHeight - 100;
     }
   };
+
   const ref = useRef<HTMLInputElement>(null);
   const [participants, setParticipants] = useState<{
     user: string;
@@ -48,25 +50,32 @@ export default function Chat({ params: { id } }: { params: { id: string } }) {
       setLoading(false);
     } catch (error) {
       console.log(error);
+      const message = getMessage(error);
+      toast({
+        title: message,
+        variant: "destructive",
+      });
     }
   }, [id, socket]);
 
   const messageSend = async () => {
-    if (!input || !id) {
+    if (!input.trim() || !id) {
       ref?.current?.focus();
       return;
     }
     try {
       const response = await sendMessages({
         chatId: id,
-        content: input,
+        content: input.trim(),
       });
       const message = response.data;
       socket?.emit("message", { message, user: participants.user });
       setMessages([...messages, message]);
       setInput("");
       ref?.current?.focus();
-      scrollToBottom();
+      setTimeout(() => {
+        scrollToBottom();
+      }, 500);
     } catch (error) {
       console.log(error);
       const message = getMessage(error);
@@ -88,6 +97,9 @@ export default function Chat({ params: { id } }: { params: { id: string } }) {
   const messageReceived = useCallback(
     (message: IMessage) => {
       setMessages([...messages, message]);
+      setTimeout(() => {
+        scrollToBottom();
+      }, 500);
     },
     [messages],
   );
@@ -103,45 +115,51 @@ export default function Chat({ params: { id } }: { params: { id: string } }) {
           <Loader className="animate-spin" />
         </div>
       ) : (
-        <div className="mt-4 flex max-h-[60vh] min-h-full flex-col gap-1 lg:mt-0">
-          <div className="flex max-h-[60vh] min-h-[60vh] w-full flex-col gap-1 overflow-x-auto px-1 pt-3">
-            {!!messages.length &&
-              messages?.map((message) => (
-                <div key={message?.id} className="flex">
-                  <p
-                    className={`${message?.sender === participants?.recruiter ? "ms-auto rounded-l-3xl rounded-br rounded-tr-3xl bg-primaryColor text-white" : "rounded-r-3xl rounded-bl rounded-tl-3xl bg-foreground/10"} max-w-[60%] break-words px-4 py-2 text-sm`}
-                  >
-                    <span className="overflow-hidden">{message?.content}</span>
-                    <span
-                      className={`flex flex-grow justify-end text-sm text-foreground/50 ${message?.sender === participants?.recruiter ? "text-white/70" : ""}`}
+        <div className="mt-4 flex min-h-full flex-col gap-1 lg:mt-0">
+          <div className="flex h-full w-full flex-col justify-between gap-1 overflow-x-hidden px-3 pt-3">
+            {!!messages.length && (
+              <div
+                ref={divRef}
+                className="overflow0-y-auto flex max-h-[60vh] min-h-[60vh] flex-col gap-2 overflow-x-hidden scroll-smooth px-1 pb-2"
+              >
+                {messages?.map((message) => (
+                  <div key={message?.id} className="flex">
+                    <p
+                      className={`${message?.sender === participants?.recruiter ? "ms-auto rounded-l-3xl rounded-br rounded-tr-3xl bg-primaryColor text-white" : "rounded-r-3xl rounded-bl rounded-tl-3xl bg-foreground/10"} w-auto max-w-[400px] break-words break-all px-4 py-2 text-sm`}
                     >
-                      {formatDistanceToNow(new Date(message?.createdAt))}
-                    </span>
-                  </p>
-                </div>
-              ))}
-            <div className="mt-5" ref={divRef} />
+                      <span className="">{message?.content}</span>
+                      <span
+                        className={`flex flex-grow justify-end text-sm text-foreground/50 ${message?.sender === participants?.recruiter ? "text-white/70" : ""}`}
+                      >
+                        {formatDistanceToNow(new Date(message?.createdAt))}
+                      </span>
+                    </p>
+                  </div>
+                ))}
+              </div>
+            )}
             {!messages.length && (
-              <div className="flex flex-grow items-center justify-center text-foreground/50">
+              <div className="flex max-h-[60vh] min-h-[60vh] flex-grow items-center justify-center text-foreground/50">
                 <span>no message&apos;s</span>
               </div>
             )}
-          </div>
-          <div className="mt-5 flex w-full gap-2">
-            <Input
-              className=""
-              ref={ref}
-              onChange={(e) => setInput(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  messageSend();
-                }
-              }}
-              value={input}
-            />
-            <Button onClick={messageSend} className="h-12">
-              send
-            </Button>
+            <div className="mt-5 flex w-full gap-2">
+              <Input
+                placeholder="Type here..."
+                className="mb-2 shadow-md shadow-foreground/20 dark:shadow-none"
+                ref={ref}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    messageSend();
+                  }
+                }}
+                value={input}
+              />
+              <Button onClick={messageSend} className="h-12">
+                <Send size={17} />
+              </Button>
+            </div>
           </div>
         </div>
       )}
