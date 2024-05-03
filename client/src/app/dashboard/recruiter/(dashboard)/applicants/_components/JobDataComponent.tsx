91d86cd2a2af832c7f4cad91ba4e90b483cb5fa2
ajmal-j <ajmaljaleel7860@gmail.com>
2024-05-03 15:33:16 +0530

    feat : job action.

--- client/src/app/dashboard/recruiter/(dashboard)/applicants/_components/JobDataComponent.tsx
+++ client/src/app/dashboard/recruiter/(dashboard)/applicants/_components/JobDataComponent.tsx
@@ -3,69 +3,72 @@ import { Button } from "@/components/ui/button";
 import { formatMoney } from "@/lib/utils";
 import { IJob } from "@/types/jobs";
 import { formatDistanceToNow } from "date-fns";
-
+import Link from "next/link";
 
 export function JobDataComponent({ jobData }: { jobData: IJob }) {
-    return (
-      <div className="mb-10">
-        <div className="mb-7 flex items-start justify-between">
-          <div>
-            <h1 className="mb-2 text-3xl font-bold lg:text-4xl">
-              {jobData.title}
-            </h1>
-            <span className="text-sm text-foreground/70">
-              {jobData.officeLocation
-                ? `${jobData.officeLocation}/${jobData.workSpace}`
-                : jobData.workSpace}
-            </span>
+  return (
+    <div className="mb-10">
+      <div className="mb-7 flex items-start justify-between">
+        <div>
+          <h1 className="mb-2 text-3xl font-bold lg:text-4xl">
+            {jobData.title}
+          </h1>
+          <span className="text-sm text-foreground/70">
+            {jobData.officeLocation
+              ? `${jobData.officeLocation}/${jobData.workSpace}`
+              : jobData.workSpace}
+          </span>
+        </div>
+        <Link href={`/dashboard/recruiter/jobs/${jobData.id}`}>
+          <Button variant="outline">view job</Button>
+        </Link>
+      </div>
+      <h1 className="text-xl font-semibold">Job detail&apos;s</h1>
+      <div className="mt-5 flex flex-col gap-2">
+        <Separator className="h-[2px] bg-foreground/50" />
+        <div className="flex items-center">
+          <div className="w-full flex-1">
+            <h1 className="text-sm text-foreground/70">Posted</h1>
+            <span>{formatDistanceToNow(new Date(jobData.createdAt))}</span>
           </div>
-          <div>
-            <Button variant="outline">action</Button>
+          <div className="flex-1">
+            <h1 className="text-sm text-foreground/70">Applicant&apos;s</h1>
+            <span>{jobData.applicants}</span>
           </div>
         </div>
-        <h1 className="text-xl font-semibold">Job detail&apos;s</h1>
-        <div className="mt-5 flex flex-col gap-2">
-          <Separator className="h-[2px] bg-foreground/50" />
-          <div className="flex items-center">
-            <div className="w-full flex-1">
-              <h1 className="text-sm text-foreground/70">Posted</h1>
-              <span>{formatDistanceToNow(new Date(jobData.createdAt))}</span>
-            </div>
-            <div className="flex-1">
-              <h1 className="text-sm text-foreground/70">Applicant&apos;s</h1>
-              <span>{jobData.applicants}</span>
-            </div>
+        <Separator className="h-[2px] bg-foreground/50" />
+        <div className="flex items-center">
+          <div className="w-full flex-1">
+            <h1 className="text-sm text-foreground/70">Type</h1>
+            <span>{jobData.type}</span>
           </div>
-          <Separator className="h-[2px] bg-foreground/50" />
-          <div className="flex items-center">
-            <div className="w-full flex-1">
-              <h1 className="text-sm text-foreground/70">Type</h1>
-              <span>{jobData.type}</span>
-            </div>
-            <div className="flex-1">
-              <h1 className="text-sm text-foreground/70">Status</h1>
-              <span>{jobData.status}</span>
-            </div>
+          <div className="flex-1">
+            <h1 className="text-sm text-foreground/70">Status</h1>
+            <span
+              className={`${jobData.status === "closed" && "text-red-600"}`}
+            >
+              {jobData.status}
+            </span>
           </div>
-          <Separator className="h-[2px] bg-foreground/50" />
-          <div className="flex items-center">
-            <div className="w-full flex-1">
-              <h1 className="text-sm text-foreground/70">Pay</h1>
-              <span>
-                <span className="me-2 text-foreground/70">
-                  {jobData?.pay?.rate} :
-                </span>
-                ₹ {jobData?.pay?.minimum && formatMoney(jobData?.pay?.minimum)} -{" "}
-                {formatMoney(jobData?.pay?.maximum)}
+        </div>
+        <Separator className="h-[2px] bg-foreground/50" />
+        <div className="flex items-center">
+          <div className="w-full flex-1">
+            <h1 className="text-sm text-foreground/70">Pay</h1>
+            <span>
+              <span className="me-2 text-foreground/70">
+                {jobData?.pay?.rate} :
               </span>
-            </div>
-            <div className="flex-1">
-              <h1 className="text-sm text-foreground/70">Opening&apos;s</h1>
-              <span>{jobData.openings}</span>
-            </div>
+              ₹ {jobData?.pay?.minimum && formatMoney(jobData?.pay?.minimum)} -{" "}
+              {formatMoney(jobData?.pay?.maximum)}
+            </span>
+          </div>
+          <div className="flex-1">
+            <h1 className="text-sm text-foreground/70">Opening&apos;s</h1>
+            <span>{jobData.openings}</span>
           </div>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
