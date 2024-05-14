3a0fc2fca14ce3c272e6ddb136e788d8418f1abf
ajmal-j <141622726+ajmal-j@users.noreply.github.com>
2024-05-14 12:13:53 +0530

    Update layout.tsx

--- client/src/app/(auth)/login/layout.tsx
+++ client/src/app/(auth)/login/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export const metadata = {
   title:"Login | Carreralink",
-  origin:"origin-when-cross-origin",
+  referrer:"origin",
 }
 
 export default function Layout({ children }: { children: ReactNode }) {
