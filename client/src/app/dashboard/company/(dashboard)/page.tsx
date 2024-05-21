19c225b311eefa2e4c1b11ad49d83ad56c7ce2f0
ajmal-j <ajmaljaleel7860@gmail.com>
2024-05-22 00:19:18 +0530

    Chore : removed duplicate recent job listing.

--- client/src/app/dashboard/company/(dashboard)/page.tsx
+++ client/src/app/dashboard/company/(dashboard)/page.tsx
@@ -101,55 +101,6 @@ function JobsData({ recentJobs }: { recentJobs: IJob[] }) {
           </div>
         </div>
       </div>
-      <div className="flex-grow rounded-md border py-2">
-        <h1 className="mb-3 text-center text-xl font-semibold">
-          Recent Job&apos;s
-        </h1>
-        <div className="flex flex-col gap-2 px-3">
-          {recentJobs.map((job) => (
-            <div
-              key={job.id}
-              className="flex gap-3 rounded-md px-3 py-1 hover:bg-foreground/5"
-            >
-              <Link
-                href={`/dashboard/company/jobs/${job._id}`}
-                className="flex w-full flex-col"
-              >
-                <h2 className="mb-1 text-lg">{job.title}</h2>
-                <p className="flex items-center gap-1 text-sm capitalize text-foreground/70">
-                  <BackpackIcon className="size-[16px]" />
-                  {job.type}
-                </p>
-                {job.officeLocation && (
-                  <p className="flex items-center gap-1 text-sm text-foreground/70">
-                    <MapPin size={17} className="text-foreground/70" />
-                    {job.officeLocation}
-                  </p>
-                )}
-                <p className="flex items-center gap-1">
-                  <HandCoins size={17} className="text-foreground/70" />
-                  <span>
-                    {job.pay.rate} : ₹ {formatMoney(job.pay.minimum)} -{" "}
-                    {formatMoney(job.pay.maximum)}
-                  </span>
-                </p>
-              </Link>
-              <div className="flex flex-col items-end gap-1">
-                <div className="my-auto flex gap-1 text-sm">
-                  <span>{job.applicants}</span>
-                  <span>applicant&apos;s</span>
-                </div>
-                <p className="ms-auto flex items-center gap-1 text-sm text-foreground/60">
-                  <span className="text-nowrap">
-                    {formatDistanceToNow(job.createdAt)}
-                  </span>
-                  <ClockIcon className="size-[12px]" />
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
     </div>
   );
 }
