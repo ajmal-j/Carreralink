07f797d4549f7cb9b30c4e5f541e1f4aba5af763
ajmal-j <ajmaljaleel7860@gmail.com>
2024-05-22 15:27:24 +0530

    Add : production env

--- client/src/Providers/google-provider.tsx
+++ client/src/Providers/google-provider.tsx
@@ -1,20 +1,13 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
-console.log(clientId, "google client id ----------------------");
+
 export default function GoogleProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <GoogleOAuthProvider
-      // added this because of its not getting from the container also it will not work with any other domain google will restrict it.
-      clientId={
-        "444055010869-ie946hi941u1hcsf1on8vp6a33eqiggs.apps.googleusercontent.com"
-      }
-    >
-      {children}
-    </GoogleOAuthProvider>
+    <GoogleOAuthProvider clientId={clientId}>{children}</GoogleOAuthProvider>
   );
 }
